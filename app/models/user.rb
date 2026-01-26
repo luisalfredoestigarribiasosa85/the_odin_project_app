592cf37bc59348721a8f46ebce00ad8c4733df3a
luisalfredoestigarribiasosa85 <luisalfredoestigarribiasosa85@gmail.com>
2026-01-26 18:51:09 -0300

    feat: Implement User model with associations, validations, Devise authentication, and a custom chat room method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :cars
+  has_many :chat_rooms_as_buyer, class_name: 'ChatRoom', foreign_key: 'buyer_id', dependent: :destroy
+  has_many :chat_rooms_as_seller, class_name: 'ChatRoom', foreign_key: 'seller_id', dependent: :destroy
+  has_many :messages, dependent: :destroy
   has_one_attached :avatar
   has_rich_text :bio
   validates :username, :email, :password, presence: true
@@ -7,4 +10,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def chat_rooms
+    ChatRoom.where("buyer_id = ? OR seller_id = ?", id, id).order(updated_at: :desc)
+  end
 end
