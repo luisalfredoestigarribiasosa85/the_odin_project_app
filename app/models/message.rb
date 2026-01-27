16d6b4a44e39f841f9509047ae891ca80513db0d
luisalfredoestigarribiasosa85 <luisalfredoestigarribiasosa85@gmail.com>
2026-01-26 21:43:43 -0300

    feat: Implement real-time message display and broadcasting in chat rooms using Turbo Streams.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :chat_room
   belongs_to :user
+
+  # Aqui ocurre la magia de ActionCable
+  after_create_commit -> {
+    broadcast_append_to chat_room,
+    target: "messages" }
 end
